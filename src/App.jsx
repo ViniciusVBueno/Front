7e6416e645213eeec95501759a94c39f087b8eac
Will Amaral <willian.amaral@thoughtworks.com>
2024-07-19 20:03:27 -0300

    change deployment to cloudflare pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,21 @@ import Dia from './pages/dia/Dia'
 import Login from './pages/login/Login'
 import Description from './pages/description/Description'
 
-const router = createBrowserRouter(
-  [
-    {
-      index: true,
-      path: '/:date?',
-      element: <Dia />,
-    },
-    {
-      path: '/login',
-      element: <Login />,
-    },
-    {
-      path: '/:date/:taskId', //'/tarefa/:date/:taskId'
-      element: <Description />,
-    },
-  ],
-  { basename: '/todo-app' }
-)
+const router = createBrowserRouter([
+  {
+    index: true,
+    path: '/:date?',
+    element: <Dia />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/:date/:taskId', //'/tarefa/:date/:taskId'
+    element: <Description />,
+  },
+])
 
 function App() {
   return <RouterProvider router={router} />
